7b6ff1d342fd3b8969a361c56be4eda49eac0565
Tanu Malik <tmalik@datastar.hep.anl.gov>
2015-10-02 17:10:35 -0500

    Another working but buggy

--- RTree/AFJoin/AFJoin.cpp
+++ RTree/AFJoin/AFJoin.cpp
@@ -11,13 +11,14 @@
 #include "RTree.h"
 
 #define MeshSize 100
-#define XSize 10
+//#define XSize 10
 
 #define TBegin 10
 
 using namespace std;
 
 typedef int ValueType;
+int XSize = 10;
 
 struct Cell
 {
@@ -39,7 +40,6 @@ struct Cell
 
 struct Cell S[MeshSize], T[MeshSize];
 std::deque<int> N;
-std::deque<int> seed_list;
 
 int intersects;
 int nhbors_cnt = 0;
@@ -90,81 +90,89 @@ bool findID(std::deque<int>L,int id)
 	return true;
 }
 
+bool findList(int id, std::deque<int> N, std::deque<int> intersection_list, std::deque<int> visited_list)
+{
+	if (!findID(N,id) && !findID(intersection_list,id) && !findID(viisted_list,id))
+		return true;
+	else 
+		return false;
+}
+
 //determine neighbors
-void Neighbors(int id)
+void Neighbors(int id, std::deque<int> intersection_list,std::deque<int> visited_list)
 {
    // is id a corner
    if ((id == 0) || (id == XSize-1) || (id == (XSize* XSize) -1) || (id == (XSize-1) * XSize) )
    {
       if (id == 0)
       {
-	  if (!findID(N,id+1) && !findID(seed_list,id+1))
+	  if (!findID(N,id+1) && !findID(intersection_list,id+1))
              N.push_back(id+1);
-	  if (!findID(N,id+XSize) && !findID(seed_list,id+XSize))
+	  if (!findID(N,id+XSize) && !findID(intersection_list,id+XSize))
              N.push_back(id+XSize);
-	  if (!findID(N,id+XSize+1) && !findID(seed_list,id+XSize+1))
+	  if (!findID(N,id+XSize+1) && !findID(intersection_list,id+XSize+1))
              N.push_back(id+XSize+1);
 	  return;
       }
       if (id == XSize -1)
       {
-	  if (!findID(N,id-1) && !findID(seed_list,id-1))
+	  if (!findID(N,id-1) && !findID(intersection_list,id-1))
              N.push_back(id-1);
-	  if (!findID(N,id+XSize) && !findID(seed_list,id+XSize))
+	  if (!findID(N,id+XSize) && !findID(intersection_list,id+XSize))
              N.push_back(id+XSize);
-	  if (!findID(N,id+XSize-1) && !findID(seed_list,id+XSize-1))
+	  if (!findID(N,id+XSize-1) && !findID(intersection_list,id+XSize-1))
              N.push_back(id+XSize-1);
 	  return;
       }
       if (id == (XSize*XSize)-1)
       {
-	  if (!findID(N,id-1) && !findID(seed_list,id-1))
+	  if (!findID(N,id-1) && !findID(intersection_list,id-1))
              N.push_back(id-1);
-	  if (!findID(N,id-XSize) && !findID(seed_list,id-XSize))
+	  if (!findID(N,id-XSize) && !findID(intersection_list,id-XSize))
              N.push_back(id-XSize);
-	  if (!findID(N,id-XSize-1) && !findID(seed_list,id-XSize-1))
+	  if (!findID(N,id-XSize-1) && !findID(intersection_list,id-XSize-1))
              N.push_back(id-XSize-1);
 	  return;
       }
        if (id == (XSize-1)*XSize)
       {
-	  if (!findID(N,id+1) && !findID(seed_list,id+1))
+	  if (!findID(N,id+1) && !findID(intersection_list,id+1))
              N.push_back(id+1);
-	  if (!findID(N,id-XSize) && !findID(seed_list,id-XSize))
+	  if (!findID(N,id-XSize) && !findID(intersection_list,id-XSize))
              N.push_back(id-XSize);
-	  if (!findID(N,id-XSize+1) && !findID(seed_list,id-XSize+1))
+	  if (!findID(N,id-XSize+1) && !findID(intersection_list,id-XSize+1))
              N.push_back(id-XSize+1);
 	  return;
       }
    }
    // is id on the X boundary ignoring corners
-   if ((0 < id < (XSize-1)) || (((XSize -1)*XSize) < id  < ((XSize*XSize)-1))) 
+   if ((0 < id && id < (XSize-1)) || (((XSize -1)*XSize) < id  && id < ((XSize*XSize)-1))) 
    { 
-       if (0 < id < (XSize -1))
+       if (0 < id && id < (XSize -1))
        {
-	  if (!findID(N,id-1) && !findID(seed_list,id-1))
+	  if (!findID(N,id-1) && !findID(intersection_list,id-1))
              N.push_back(id-1);
-	  if (!findID(N,id+XSize) && !findID(seed_list,id+XSize))
+	  if (!findID(N,id+XSize) && !findID(intersection_list,id+XSize))
              N.push_back(id+XSize);
-	  if (!findID(N,id+XSize+1) && !findID(seed_list,id+XSize+1))
+	  if (!findID(N,id+XSize+1) && !findID(intersection_list,id+XSize+1))
              N.push_back(id+XSize+1);
-	  if (!findID(N,id+XSize-1) && !findID(seed_list,id+XSize-1))
+	  if (!findID(N,id+XSize-1) && !findID(intersection_list,id+XSize-1))
              N.push_back(id+XSize -1);
-	  if (!findID(N,id+1) && !findID(seed_list,id+1))
+	  if (!findID(N,id+1) && !findID(intersection_list,id+1))
              N.push_back(id+1);
           return;
        }		
-       if (((XSize -1)*XSize) < id  < ((XSize*XSize)-1))
+       if (((XSize -1)*XSize) < id  && id < ((XSize*XSize)-1))
        {
-	  if (!findID(N,id-1) && !findID(seed_list,id-1))
+	  if (!findID(N,id-1) && !findID(intersection_list,id-1))
 	     N.push_back(id-1);
-	  if (!findID(N,id-XSize) && !findID(seed_list,id-XSize))
+	  if (!findID(N,id-XSize) && !findID(intersection_list,id-XSize))
              N.push_back(id-XSize);
-	  if (!findID(N,id-XSize+1) && !findID(seed_list,id-XSize+1))
+	  if (!findID(N,id-XSize+1) && !findID(intersection_list,id-XSize+1))
              N.push_back(id-XSize+1);
-	  if (!findID(N,id-XSize-1) && !findID(seed_list,id-XSize-1))
+	  if (!findID(N,id-XSize-1) && !findID(intersection_list,id-XSize-1))
              N.push_back(id-XSize -1);
-	  if (!findID(N,id+1) && !findID(seed_list,id+1))
+	  if (!findID(N,id+1) && !findID(intersection_list,id+1))
              N.push_back(id+1);
 	  return;
        }
@@ -174,29 +182,29 @@ void Neighbors(int id)
    {
         if (id %XSize == 0)
        {
-	  if (!findID(N,id+1) && !findID(seed_list,id+1))
+	  if (!findID(N,id+1) && !findID(intersection_list,id+1))
              N.push_back(id+1);
-	  if (!findID(N,id+XSize) && !findID(seed_list,id+XSize))
+	  if (!findID(N,id+XSize) && !findID(intersection_list,id+XSize))
              N.push_back(id+XSize);
-	  if (!findID(N,id+XSize+1) && !findID(seed_list,id+XSize+1))
+	  if (!findID(N,id+XSize+1) && !findID(intersection_list,id+XSize+1))
              N.push_back(id+XSize+1);
-	  if (!findID(N,id-XSize) && !findID(seed_list,id-XSize))
+	  if (!findID(N,id-XSize) && !findID(intersection_list,id-XSize))
              N.push_back(id-XSize);
-	  if (!findID(N,id-XSize+1) && !findID(seed_list,id-XSize+1))
+	  if (!findID(N,id-XSize+1) && !findID(intersection_list,id-XSize+1))
              N.push_back(id-XSize+1);
 	  return;
        }		
        if ( id % XSize  == XSize -1)
        {
-	  if (!findID(N,id-1) && !findID(seed_list,id-1))
+	  if (!findID(N,id-1) && !findID(intersection_list,id-1))
 	     N.push_back(id-1);
-	  if (!findID(N,id-XSize) && !findID(seed_list,id-XSize))
+	  if (!findID(N,id-XSize) && !findID(intersection_list,id-XSize))
              N.push_back(id-XSize);
-	  if (!findID(N,id-XSize-1) && !findID(seed_list,id-XSize-1))
+	  if (!findID(N,id-XSize-1) && !findID(intersection_list,id-XSize-1))
              N.push_back(id-XSize-1);
-	  if (!findID(N,id+XSize-1) && !findID(seed_list,id+XSize-1))
+	  if (!findID(N,id+XSize-1) && !findID(intersection_list,id+XSize-1))
              N.push_back(id+XSize -1);
-	  if (!findID(N,id+XSize) && !findID(seed_list,id+XSize))
+	  if (!findID(N,id+XSize) && !findID(intersection_list,id+XSize))
              N.push_back(id+XSize);
 	  return;
        }
@@ -204,21 +212,21 @@ void Neighbors(int id)
    // or is id internal; numnhbrs = 8
    for (int i = 0; i < 8; i++)
    {
-	  if (!findID(N,id-1) && !findID(seed_list,id-1))
+	  if (!findID(N,id-1) && !findID(intersection_list,id-1))
 	     N.push_back(id-1);
-	  if (!findID(N,id+1) && !findID(seed_list,id+1))
+	  if (!findID(N,id+1) && !findID(intersection_list,id+1))
 	     N.push_back(id+1);
-	  if (!findID(N,id-XSize) && !findID(seed_list,id-XSize))
+	  if (!findID(N,id-XSize) && !findID(intersection_list,id-XSize))
              N.push_back(id-XSize);
-	  if (!findID(N,id-XSize-1) && !findID(seed_list,id-XSize-1))
+	  if (!findID(N,id-XSize-1) && !findID(intersection_list,id-XSize-1))
              N.push_back(id-XSize-1);
-	  if (!findID(N,id-XSize+1) && !findID(seed_list,id-XSize+1))
+	  if (!findID(N,id-XSize+1) && !findID(intersection_list,id-XSize+1))
              N.push_back(id-XSize+1);
-	  if (!findID(N,id+XSize) && !findID(seed_list,id+XSize))
+	  if (!findID(N,id+XSize) && !findID(intersection_list,id+XSize))
              N.push_back(id+XSize);
-	  if (!findID(N,id+XSize-1) && !findID(seed_list,id+XSize-1))
+	  if (!findID(N,id+XSize-1) && !findID(intersection_list,id+XSize-1))
              N.push_back(id+XSize-1);
-	  if (!findID(N,id+XSize+1) && !findID(seed_list,id+XSize+1))
+	  if (!findID(N,id+XSize+1) && !findID(intersection_list,id+XSize+1))
              N.push_back(id+XSize+1);
 	  return;
    }
@@ -265,42 +273,62 @@ int main()
 
   //AFJoin implementation
   clock_t begin = clock();
-  /*for (int i = 0; i < XSize; i++)
-  {
-  for (int j = XSize*i; j < ((i*XSize)+XSize); j++)
-  {*/
+     int next_element = 0; 
      //for a given cell of T_d findID if it intersects
-     Cell search_rect(T[0].min[0],T[0].min[1],T[0].max[0],T[0].max[1]);
+     Cell search_rect;
+	 search_rect  = Cell(T[next_element].min[0],T[next_element].min[1],T[next_element].max[0],T[next_element].max[1]);
      int seed = STree.Search(search_rect.min, search_rect.max, MySearchCallback, NULL);
      cout << "start seed " << seed << "\n";
 
-     std::deque<int>::iterator it = std::find(seed_list.begin(),seed_list.end(),seed);
-     if (it == seed_list.end())	
-     	seed_list.push_back(seed);
-
+     
      if (seed >= 0)
-     {
-	//findID Nhbrs of id
-	Neighbors(seed);
-	while (!N.empty())
-	{
-	   int id = N.front();
-	   cout << "count: " << N.size() << " id " << id << "\n";
-	   N.pop_front();
-	   int *coords = getCellCoords(id);
-	   Cell temp(coords[0],coords[1],coords[2],coords[3]);
-	   if (Overlap(&search_rect,&temp))
-	   {
-     		std::deque<int>::iterator it = std::find(seed_list.begin(),seed_list.end(),id); 
-     		if (it == seed_list.end())	
-     			seed_list.push_back(id);
+     { 
+	while (next_element < MeshSize)
+	{	
+     
+        std::queue<int> N_visited;
+     	std::deque<int> intersection_list;
+		intersection_list.push_back(seed);
+			
+		cout << "Donor element is " << next_element << "\n";
+		//findID Nhbrs of seed
+		Neighbors(seed,intersection_list,N_visited);
+		while (!N.empty())
+		{
+		   int id = N.front();
+		   cout << "count: " << N.size() << "seed " << seed << " nid " << id << "\n";
+		   N.pop_front();
+		   N_visited.push_back(id);
+		   int *coords = getCellCoords(id);
+		   Cell temp(coords[0],coords[1],coords[2],coords[3]);
+		   if (Overlap(&search_rect,&temp))
+		   {
+			std::deque<int>::iterator it = std::find(intersection_list.begin(),intersection_list.end(),id); 
+			if (it == intersection_list.end())	
+				intersection_list.push_back(id);
 
-		Neighbors(id);
-	   }
-	}
-     }	
-  /*}
-  }*/
+			Neighbors(id,intersection_list,N_visted);
+		   }
+		}
+		//get next T_d element
+		next_element = next_element + 1;
+		int *coords = getCellCoords(next_element);
+		search_rect = Cell(coords[0],coords[1],coords[2],coords[3]); 
+		for (std::deque<int>::iterator it = intersection_list.begin(); it != intersection_list.end(); ++it)
+		{
+			//std::cout << ' ' << *it;
+			coords = getCellCoords(*it);
+			Cell temp(coords[0],coords[1],coords[2],coords[3]);
+			if (Overlap(&search_rect,&temp))
+			{
+			   seed = *it;
+			   //cout << "New seed is " << seed << "\n";
+			   break;
+			}
+		}
+      }	
+      }
+  
 
   
   clock_t end = clock();
